98865c1b80f607a6e8aac2d63361dad1606ff5b8
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2022-12-01 22:36:14 +1100

    day 1 part 1

---
+++ day1/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const data = fs.readFileSync('input', 'utf-8');
+const lines = data.split(/\r?\n/);
+
+let cal = 0;
+let max = 0;
+
+lines.forEach(line => {
+	if (line === '') {
+		cal = 0;
+		return;
+	}
+
+	cal += parseInt(line);
+	if (cal > max) max = cal;
+});
+
+console.log(max);
